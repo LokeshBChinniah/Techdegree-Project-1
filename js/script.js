70f5497c09a08ca082d5f2b84c62fd482085780e
LokeshBChinniah <57022188+LokeshBChinniah@users.noreply.github.com>
2020-02-23 17:12:19 -0500

    added quotes and sources

--- js/script.js
+++ js/script.js
@@ -10,7 +10,30 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+var quotes = [
+  {
+    quote: 'You miss a 100 percent of the shots that you dont take.',
+    source: 'Wayne Gretzky'
+  },
+  {
+    quote: 'It is during our darkest moments that we must focus to see the light.',
+    source: 'Aristotle'
+  },
+  {
+    quote: 'Never let the fear of striking out keep you from playing the game.',
+    source: 'Babe Ruth'
+  },
+  {
+    quote: 'You can manipulate an opponent’s strength and use it against them.',
+    source: 'Kobe Bryant',
+    citation: 'The Mamba Mentality: How I Play',
+    year: '2018'
+  },
+  {
+    quote: 'The way to get started is to quit talking and begin doing.',
+    source: 'Walt Disney'
+  }
+  ];
 
 
 /***
